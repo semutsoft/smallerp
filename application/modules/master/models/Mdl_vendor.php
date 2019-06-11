98dcae334d9a504dac637a12cd2d0b0f7d6f1bea
dwi wahyudi <dwi_wahyudi@yahoo.com>
2019-06-11 18:07:38 +0700

    UPDATE code add new funtion and controller

--- application/modules/master/models/Mdl_vendor.php
+++ application/modules/master/models/Mdl_vendor.php
@@ -12,7 +12,19 @@ class Mdl_vendor extends Mdl_utama {
                     array('id'=>'vendor_code', 'label'=>'Code', 'visible'=>true, 'format'=>'TEXT', 'placeholder'=>'', 'required'=>false, 'col_css'=>'col-md-10'))), 
                 'vendor_name' => array('id'=>'vendor_name', 'label'=>'Name', 'key'=>true, 'visible'=>true, 'form'=>array(                                        
                     array('id'=>'vendor_name', 'label'=>'Name', 'visible'=>true, 'format'=>'TEXT', 'placeholder'=>'', 'required'=>false, 'col_css'=>'col-md-10'))), 
-                
+                'vendor_address'  => array('id'=>'vendor_address', 'label'=>'VendorAddress', 'key'=>false, 'visible'=>true, 'form'=>array(
+                                        array('id'=>'vendor_address', 'label'=>'VendorAddress', 'visible'=>true, 'format'=>'TEXT', 'placeholder'=>'Alamat', 'required'=>false, 'col_css'=>'col-md-10'))), 
+                'vendor_city'     => array('id'=>'vendor_city', 'label'=>'VendorCity', 'key'=>false, 'visible'=>true, 'form'=>array(
+                                        array('id'=>'vendor_city', 'label'=>'VendorCity', 'visible'=>true, 'format'=>'TEXT', 'placeholder'=>'Kota', 'required'=>false, 'col_css'=>'col-md-10'))), 
+                'province'          => array('id'=>'province', 'label'=>'Propinsi', 'key'=>false, 'visible'=>true, 'form'=>array(
+                                        array('id'=>'province', 'label'=>'Propinsi', 'visible'=>true, 'format'=>'TEXT', 'placeholder'=>'Propinsi', 'required'=>false, 'col_css'=>'col-md-10'))), 
+                'zip_postal'        => array('id'=>'zip_postal', 'label'=>'Kode Pos', 'key'=>false, 'visible'=>true, 'form'=>array(
+                                        array('id'=>'zip_postal', 'label'=>'Kode Pos', 'visible'=>true, 'format'=>'TEXT', 'placeholder'=>'Kode Pos', 'required'=>false, 'col_css'=>'col-md-10'))), 
+                'phone'             => array('id'=>'phone', 'label'=>'Telp.', 'key'=>false, 'visible'=>true, 'form'=>array(
+                                        array('id'=>'phone', 'label'=>'Telp.', 'visible'=>true, 'format'=>'TEXT', 'placeholder'=>'telp', 'required'=>false, 'col_css'=>'col-md-10'))), 
+                'fax'               => array('id'=>'fax', 'label'=>'Fax.', 'key'=>false, 'visible'=>true, 'form'=>array(
+                                        array('id'=>'fax', 'label'=>'Fax.', 'visible'=>true, 'format'=>'TEXT', 'placeholder'=>'fax', 'required'=>false, 'col_css'=>'col-md-10'))), 
+                        
             ),
             'idkey'     => 'vendor_code',
             'join'      => array(),
@@ -44,6 +56,12 @@ class Mdl_vendor extends Mdl_utama {
                     'no'            => $no,
                     'vendor_code'   => $row->vendor_code,
                     'vendor_name'   => $row->vendor_name,
+                    'vendor_address'   => $row->vendor_address,
+                    'vendor_city'   => $row->vendor_city,
+                    'province'      => $row->province,
+                    'zip_postal'   => $row->zip_postal,
+                    'phone'         => $row->phone,
+                    'fax'           => $row->fax,
                     'actions'       => $button
                 );
                 $no++;
